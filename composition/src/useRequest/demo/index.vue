ae45cccbecca4c97e5b9fd07ad3f006b0dc6b6dc
phpjavac <1060854705@qq.com>
2021-08-28 11:20:42 +0800

    update demo

--- composition/src/useRequest/demo/index.vue
+++ composition/src/useRequest/demo/index.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-import useRequest from "../index";
+import useRequest from '../index';
 
-const { error, loading, response } = useRequest<{ hitokoto: string }>({
+interface Hitokoto {
+  from: string;
+  hitokoto: string;
+  id: number;
+  uuid: string;
+}
+
+const { error, loading, response } = useRequest<Hitokoto>({
   url: "https://v1.hitokoto.cn/",
-  method: "post",
+  method: "get",
 });
 </script>
 <template>
   <div v-if="error">{{ error }}</div>
   <div v-else-if="loading">{{ loading }}</div>
-  <div v-else>{{ response.hitokoto }}</div>
+  <div v-else>
+    <div class="hitokoto"> {{ response.hitokoto }} - {{ response.from }}</div>
+  </div>
 </template>
