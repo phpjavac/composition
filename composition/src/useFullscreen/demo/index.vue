f8e639ca5aa8a17884f18a015f800407c07d978c
Zhaoiii <zdhzdhzdhzdhzdh@gmail.com>
2021-09-03 16:48:42 +0800

    优化 + 全屏初版

---
+++ composition/src/useFullscreen/demo/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div ref='divRef' class="divStyle">
+
+    </div>
+    <button @click='full'>点击</button>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import useFullscreen from "../index";
+import { ref } from 'vue'
+
+const divRef = ref<HTMLElement | null>(null);
+
+function full() {
+  if(divRef.value) {
+    useFullscreen(divRef.value)
+  }
+}
+
+</script>
+
+<style scoped>
+.divStyle{
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  display: inline-block;
+}
+</style>
--- composition/src/useSize/index.ts
+++ composition/src/useSize/index.ts
@@ -1,17 +1,44 @@
-import { ref } from "vue";
+import { Ref, ref } from "vue";
+import { safeOnMounted } from "../utils";
+import { debounce } from "loadsh";
 
-const useSize = (target: HTMLElement, callback?: () => void) => {
+
+/**
+ * 
+ * @param target 
+ * @param callback 
+ * @returns 
+ */
+
+const useSize = (target: Ref<HTMLElement | null>, callback?) => {
+    const instance = ref(false);
     const size = ref({
-        width: target.clientWidth,
-        height: target.clientHeight
+        width: 0,
+        height: 0
     })
+    let callbackVar: Function|undefined = undefined
+    const debounceCallback = debounce(() => { callbackVar && callbackVar() }, 300,{ maxWait: 400 })
     const resizeObserver = new ResizeObserver(() => {
-        size.value.height = target.clientHeight
-        size.value.width = target.clientWidth
-        callback && callback();
+        size.value.height = target.value.clientHeight
+        size.value.width = target.value.clientWidth
+        if (instance.value) {
+            callbackVar = callback
+            debounceCallback()
+        }
+        instance.value = true;
     });
+    const hook = () => {
+        if (target.value) {
+            resizeObserver.observe(target.value);
+        } else {
+            console.error('元素未挂载')
+        }
 
-    resizeObserver.observe(target);
+    }
+    safeOnMounted(
+        hook
+    )
+
     return size.value;
 }
 
