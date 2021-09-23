7ba53a30cf81e0c0abaed01f95901804fc4d6291
Zhaoiii <zdhzdhzdhzdhzdh@gmail.com>
2021-09-23 18:18:57 +0800

    init:debounce

--- src/useInterval/index.ts
+++ src/useInterval/index.ts
@@ -1,5 +1,17 @@
-const useInterval = () => {
+import { ref, Ref } from "vue";
+import { debounce  } from "lodash";
 
+type Fn = (...args: any) => any
+
+interface DebounceOptions{
+    wait: number;
+    leading: boolean;
+    traling: boolean;
+}
+
+function useDebounceFn<T extends Fn>(Fn: T, options: DebounceOptions) {
+    const fn:Ref<T> = ref(Fn)
 };
+// useDebounceFn(()=>{console.log('ddd');return 2}, {wait:1,leading:false,traling:false})
 
-export default useInterval;
+export default useDebounceFn;
